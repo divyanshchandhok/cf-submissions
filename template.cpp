10f5e09ee08482c05b5d82b088249cc06b4c8237
Divyansh Chandhok <betterthanyou155@gmail.com>
2026-06-16 05:47:06 +0530

    add template.cpp and debug.cpp helper scripts

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+#define int long long
+
+int ceil_div(int a, int b){
+    return (a + b - 1) / b;
+}
+
+bool is_int(float a){
+    return floor(a) == a;
+}
+
+void solve() {
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    
+    while (t--) 
+    solve();
+}
